72212bde0c71fe83aed62f873daf294e78cb6489
Amir <amirrezaie.dev@gmail.com>
2024-08-25 11:20:00 +0330

    feat(ui): add header, navigation menu with HTML, CSS, and JavaScript

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,36 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navClose = document.getElementById('nav-close'),
+      navToggle = document.getElementById('nav-toggle')
+
+
+/* Show Menu */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}    
+
+/* Hidden Menu */
+if(navClose){
+    navClose.addEventListener('click',()=>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
 
 
 /*=============== REMOVE MENU MOBILE ===============*/
+
+const navLink = document.querySelectorAll('.nav__link')
+
+
+const linkAction = () => {
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+
+navLink.forEach(n => n.addEventListener('click',linkAction))
 
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
